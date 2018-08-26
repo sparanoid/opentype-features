b0063e68c6f23ce54c9b9e7d9da767a6776827d5
sparanoid <t@sparanoid.com>
2018-08-26 21:07:43 +0800

    feat(js): add tooltip support

--- _app/assets/_js/user.js
+++ _app/assets/_js/user.js
@@ -1,4 +1,5 @@
 // Custom user scripts
+// TypeKit fonts
 (function(d) {
   var config = {
     kitId: 'yfh1rhp',
@@ -7,3 +8,15 @@
   },
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
+
+// Tooltips
+document.addEventListener('DOMContentLoaded', function(){
+  var tags = document.querySelectorAll('.tag, abbr, .demo > span');
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    new Tooltip(tag, {
+      placement: tag.parentNode.classList.contains('demo-multiline') ? 'left' : 'top',
+      container: 'body'
+    });
+  }
+});
